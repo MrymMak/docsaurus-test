39cb03cee4ce11c35bd617ef3dee452902b76fbf
Meryem Makrane <m.makrane@arcinfo.com>
2023-09-14 19:16:45 +0200

    fix edit urls in js config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/MrymMak/docsaurus-test/tree/main/docs/',
+            'https://github.com/MrymMak/docsaurus-test/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/MrymMak/docsaurus-test/tree/main/blog/',
+            'https://github.com/MrymMak/docsaurus-test/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
